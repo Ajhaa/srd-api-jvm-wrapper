039233204f2bbe61f3ee8fb9451848dbd1c1494b
atte <atte.h.1998@gmail.com>
2020-10-11 16:53:03 +0300

    Add test for choices

--- src/test/java/JavaTest.java
+++ src/test/java/JavaTest.java
@@ -7,31 +7,37 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class JavaTest {
     SRDApi api = new SRDApi("http://localhost:3000");
+    Class wizard = api.getClasses().get("wizard");
 
     @Test
-    public void apiWorksInJava() {
+    public void testGetClass() {
         Class bard = api.getClasses().get("bard");
         assertEquals("Bard", bard.getName());
     }
 
     @Test
-    public void lazyAbilityScoresWork() {
-        Class wizard = api.getClasses().get("wizard");
+    public void testSavingThrows() {
         List<AbilityScore> saves = wizard.getSavingThrows();
         assertEquals("INT", saves.get(0).getName());
     }
 
     @Test
-    public void lazyProficienciesWork() {
-        Class wizard = api.getClasses().get("wizard");
+    public void testProficiencies() {
         List<Proficiency> profs = wizard.getProficiencies();
         assertEquals("daggers", profs.get(0).getIndex());
     }
 
     @Test
-    public void lazySubclassesWork() {
-        Class wizard = api.getClasses().get("wizard");
+    public void testSubclasses() {
         List<Subclass> sc = wizard.getSubclasses();
         assertEquals("evocation", sc.get(0).getIndex());
+    }
+
+    @Test
+    public void testProficiencyChoices() {
+        Choice<Proficiency> choice = wizard.getProficiencyChoices().get(0);
+
+        assertEquals(2, choice.getChoose());
+        assertEquals("skill-arcana", choice.getFrom().get(0).getIndex());
     }
 }
